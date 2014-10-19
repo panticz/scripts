865b80b0e04721bb37ee2a15e69482c57bf4e3f1
panticz <mail@konczalski.de>
2014-10-19 16:31:46 +0200

    rewrite auto update non security packages

--- enable_auto_update.sh
+++ enable_auto_update.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-sed -i '4s|//| |g' /etc/apt/apt.conf.d/50unattended-upgrades
+. /etc/os-release
+
+# enable update on debian
+if [ "${ID}" == "debian" ]; then
+  sed -i 's|//      "o=Debian,a=stable";|        "o=Debian,a=stable";|g' /etc/apt/apt.conf.d/50unattended-upgrades
+  sed -i 's|//      "o=Debian,a=stable-updates";|        "o=Debian,a=stable-updates";|g' /etc/apt/apt.conf.d/50unattended-upgrades
+fi
+
+# enable update on ubuntu
+if [ "${ID}" == "ubuntu" ]; then
+  sed -i '4s|//| |g' /etc/apt/apt.conf.d/50unattended-upgrades
+fi
+
 #sed -i 's|//Unattended-Upgrade::Mail|Unattended-Upgrade::Mail|g' /etc/apt/apt.conf.d/50unattended-upgrades
 sed -i 's|APT::Periodic::Download-Upgradeable-Packages "0";|APT::Periodic::Download-Upgradeable-Packages "1";|g' /etc/apt/apt.conf.d/10periodic
 sed -i 's|APT::Periodic::AutocleanInterval "0";|APT::Periodic::AutocleanInterval "7";|g' /etc/apt/apt.conf.d/10periodic
