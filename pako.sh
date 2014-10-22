3e09f90df4bb8943c3f321cb8000c26a775eac74
panticz <mail@konczalski.de>
2014-10-22 09:30:20 +0200

    add comments

--- pako.sh
+++ pako.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# configure gnome
+# configure clock
 gsettings set com.canonical.indicator.datetime show-date true
 gsettings set com.canonical.indicator.datetime show-day true
+
+# configure workspaces
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 2
+
+# configure nautilus
 gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
 gsettings set org.gnome.nautilus.list-view default-zoom-level 'smallest'
