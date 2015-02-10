e523be7e3dbe828ac165e25c422d6d2b74006ddc
panticz <mail@konczalski.de>
2015-02-10 10:58:39 +0100

    use service to restart daemon

--- disable_ssh_password_authentication.sh
+++ disable_ssh_password_authentication.sh
@@ -2,4 +2,4 @@
 
 sed -i 's|[#]*PasswordAuthentication yes|PasswordAuthentication no|g' /etc/ssh/sshd_config
 sed -i 's|UsePAM yes|UsePAM no|g' /etc/ssh/sshd_config
-/etc/init.d/ssh restart
+service ssh restart
