255c343a842c6e08348fa0e1167d391c2e7fe15e
panticz <mail@konczalski.de>
2016-04-13 13:25:43 +0200

    add script to compile Squid with SSL support

---
+++ compile.squid.with.ssl.support.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# add Debian Wheezy backports repository
+cat <<EOF> /etc/apt/sources.list.d/wheezy-backports.list
+deb http://ftp.de.debian.org/debian wheezy-backports main
+deb-src http://ftp.de.debian.org/debian wheezy-backports main
+EOF
+ 
+# update repository list
+apt-get update
+ 
+# install required dev packages
+apt-get install -y wget openssl devscripts build-essential libssl-dev
+ 
+# install debian squid3 source code
+apt-get source -y squid3
+ 
+# install all required dependeny packages
+apt-get build-dep -y squid3
+ 
+# enable SSL support
+#vi debian/rules
+#root@jessie:~/squid3-3.4.8# diff /tmp/rules debian/rules 
+#46c46,48
+#< 		--with-default-user=proxy
+#---
+#> 		--with-default-user=proxy \
+#> 		--enable-ssl \
+#> 		--with-open-ssl="/etc/ssl/openssl.cnf"
+wget -q http://dl.panticz.de/squid/squid3-3.4.8_enable_ssl.diff -O - | patch -p2 squid3-3.4.8/debian/rules
+ 
+# build packages
+cd squid3-3.4.8
+debuild -us -uc
