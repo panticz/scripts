fd852ea6cdb1104127d4a2825b6f5938a9da2b1c
panticz <mail@konczalski.de>
2019-10-21 11:17:25 +0200

    Use basic wget instead of curl

--- run_linpack.sh
+++ run_linpack.sh
@@ -3,7 +3,7 @@ $!/bin/bash
 URL=https://software.intel.com/sites/default/files/managed/e4/1c/l_mklb_p_2019.5.004.tgz
 
 # Download and extract LINPACK
-[ ! -d /tmp/l_mklb_p_* ] && curl -s ${URL} | tar -xz -C /tmp
+[ ! -d /tmp/l_mklb_p_* ] && wget -qO- ${URL} | tar -xz -C /tmp
 
 # Run LINPACK
 (
